84017e8a01df715e762fdbe7cda95619d2f10bd8
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2025-04-24 22:23:05 +0900

    Create sample web app

---
+++ sample/src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function IndexPage() {
+  return (
+    <div>
+      <h1>Welcome to the Next.js App!</h1>
+      <p>This is a simple Next.js application.</p>
+      <p>Explore the features and enjoy your stay!</p>
+    </div>
+  );
+};
