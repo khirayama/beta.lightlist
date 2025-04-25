1fb49b11ac616d3bd609aa4b41468054c4ad60fa
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2025-04-25 21:56:23 +0900

    Create register page

--- sample/src/pages/index.tsx
+++ sample/src/pages/index.tsx
@@ -1,9 +1,13 @@
 export default function IndexPage() {
   return (
     <div>
-      <h1>Welcome to the Next.js App!</h1>
-      <p>This is a simple Next.js application.</p>
-      <p>Explore the features and enjoy your stay!</p>
+      <h1>Lightlist</h1>
+      <div>
+        <a href="/register">Register</a>
+      </div>
+      <div>
+        <a href="/login">Login</a>
+      </div>
     </div>
   );
-};
+}
