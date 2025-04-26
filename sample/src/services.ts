0bdb0f518e82a6ffb67a5329bb4e8c15f6d43863
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2025-04-26 22:48:31 +0900

    Implement refreshToken

--- sample/src/services.ts
+++ sample/src/services.ts
@@ -1,6 +1,48 @@
 import axios from "axios";
+import { saveSession, loadSession } from "./utils";
 
 const API_URL = "http://localhost:3000";
+
+const refreshToken = (() => {
+  let isRefreshing = null;
+
+  return () => {
+    if (isRefreshing) {
+      return isRefreshing;
+    }
+
+    isRefreshing = new Promise((resolve, reject) => {
+      const s = loadSession();
+
+      if (s.session.expiresAt - Date.now() / 1000 < 600) {
+        isRefreshing = true;
+
+        axios
+          .post(
+            "/api/auth/refresh",
+            {
+              refreshToken: s.session.refreshToken,
+            },
+            {
+              baseURL: API_URL,
+            }
+          )
+          .then((res) => {
+            isRefreshing = null;
+            saveSession(res.data);
+            return resolve(res.data);
+          })
+          .catch((err) => {
+            reject(err);
+            throw new Error(err.response.data.error);
+          });
+      } else {
+        resolve(s);
+      }
+    });
+    return isRefreshing;
+  };
+})();
 
 export function register(credentials: { email: string; password: string }) {
   return axios
@@ -8,6 +50,7 @@ export function register(credentials: { email: string; password: string }) {
       baseURL: API_URL,
     })
     .then((res) => {
+      saveSession(res.data);
       return res.data;
     })
     .catch((err) => {
