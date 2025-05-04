a4abb68e7d7e03042cbbefd4622ee680e2e11e55
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2025-05-04 22:09:01 +0900

    Remove profile endpoint

---
+++ api/src/pages/api/auth/change-email.ts
@@ -0,0 +1,7 @@
+// supabase.auth.setSession({
+//   access_token: accessToken,
+//   refresh_token: "dummy",
+// });
+// const { error } = await supabase.auth.updateUser({
+//   email: req.body.email,
+// });
