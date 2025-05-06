a022d222b39f35f9deb476258a9db4d24539f7f7
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2025-05-06 16:36:20 +0900

    Impelement createSupabaseAdminClient

--- api/src/common/supabase.ts
+++ api/src/common/supabase.ts
@@ -8,3 +8,10 @@ export function createSupabaseClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   );
 }
+
+export function createSupabaseAdminClient() {
+  return createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!,
+  );
+}
