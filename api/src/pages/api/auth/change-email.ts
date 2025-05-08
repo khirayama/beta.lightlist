fba917565fd505a20378a993e945caafb5b5b09a
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2025-05-09 08:54:51 +0900

    Remove needless files

--- api/src/pages/api/auth/change-email.ts
+++
@@ -1,7 +0,0 @@
-// supabase.auth.setSession({
-//   access_token: accessToken,
-//   refresh_token: "dummy",
-// });
-// const { error } = await supabase.auth.updateUser({
-//   email: req.body.email,
-// });
