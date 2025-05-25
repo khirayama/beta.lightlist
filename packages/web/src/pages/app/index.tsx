3f4c7bdaf1faba39887e8a1667d6daab017b8c4b
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2025-05-25 09:52:17 +0900

    Use navigation/react and TaskList app component

--- packages/web/src/pages/app/index.tsx
+++ packages/web/src/pages/app/index.tsx
@@ -20,18 +20,14 @@ import { CSS } from "@dnd-kit/utilities";
 import {
   init,
   insertTaskList,
-  updateTaskList,
   deleteTaskList,
-  insertTask,
-  updateTask,
-  sortTasks,
-  moveTask,
-  clearCompletedTasks,
   moveTaskList,
   store,
-  type Task,
   type TaskList,
 } from "sdk";
+
+import { NavigationProvider } from "navigation/react";
+import { TaskList as TaskListComponent } from "appcomponents/TaskList";
 
 /* Features
  * [x] タスクリストの追加
@@ -48,215 +44,39 @@ import {
  * [x] タスクの更新(完了、テキスト、日付)
  */
 
-// MEMO: handleXXは、component用のevent handlerの命名で、(props, state, payload)を持つ
-type ComponentEventHandler<P, S, V> = (props: P, state: S, val?: V) => void;
-
-interface TaskTextInputProps {
-  task: Task;
-  handleBlur: ComponentEventHandler<TaskTextInputProps, { text: string }, null>;
-}
-
-function TaskTextInput(props: TaskTextInputProps) {
-  const task = props.task;
-  const [text, setText] = useState(task.text);
-
-  useEffect(() => {
-    setText(task.text);
-  }, [task.text]);
-
-  return (
-    <input
-      type="text"
-      value={text}
-      onChange={(e) => {
-        const newText = e.currentTarget.value;
-        setText(newText);
-      }}
-      onBlur={(e) => props.handleBlur(props, { text }, null)}
-    />
-  );
-}
-
-interface TaskListComponentProps {
-  taskList: TaskList;
-}
-
-function TaskListComponent(props: TaskListComponentProps) {
-  const taskList = props.taskList;
-
-  const [taskListName, setTaskListName] = useState(taskList.name);
-  const [newTaskText, setNewTaskText] = useState("");
-
-  const sensors = useSensors(
-    useSensor(PointerSensor),
-    useSensor(KeyboardSensor, {
-      coordinateGetter: sortableKeyboardCoordinates,
-    })
-  );
-
-  const handleDragEnd = (e: DragEndEvent) => {
-    const { active, over } = e;
-
-    if (active && over && active.id !== over.id) {
-      const from = taskList.tasks.findIndex((t) => t.id === active.id);
-      const to = taskList.tasks.findIndex((t) => t.id === over.id);
-      moveTask(taskList.id, from, to);
-    }
+const routes: {
+  [path: string]: {
+    isDrawerOpen: boolean;
+    isSharingSheetOpen: boolean;
+    isDatePickerSheetOpen: boolean;
   };
-
-  return (
-    <section>
-      <header>
-        <input
-          type="text"
-          value={taskListName}
-          onChange={(e) => {
-            const newName = e.currentTarget.value;
-            setTaskListName(newName);
-          }}
-          onBlur={(e) => {
-            if (taskListName !== taskList.name) {
-              updateTaskList({
-                ...taskList,
-                name: taskListName,
-              });
-            }
-          }}
-        />
-
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            if (newTaskText !== "") {
-              insertTask(
-                taskList.id,
-                { text: newTaskText },
-                taskList.tasks.length
-              );
-              setNewTaskText("");
-            }
-          }}
-        >
-          <input
-            type="text"
-            placeholder="New Task"
-            value={newTaskText}
-            onChange={(e) => {
-              setNewTaskText(e.currentTarget.value);
-            }}
-          />
-          <button>Add Task</button>
-        </form>
-        <p>
-          {window.location.origin +
-            window.location.pathname +
-            `/share?shareCodes=${taskList.shareCode}`}
-        </p>
-        <button
-          onClick={() => {
-            sortTasks(taskList.id);
-          }}
-        >
-          Sort tasks
-        </button>
-        <button
-          onClick={() => {
-            clearCompletedTasks(taskList.id);
-          }}
-        >
-          Clear completed tasks
-        </button>
-      </header>
-      <DndContext
-        sensors={sensors}
-        collisionDetection={closestCenter}
-        modifiers={[restrictToVerticalAxis]}
-        onDragEnd={handleDragEnd}
-      >
-        <SortableContext
-          items={taskList.tasks}
-          strategy={verticalListSortingStrategy}
-        >
-          {taskList.tasks.map((task) => {
-            return (
-              <TaskListItemComponent
-                key={task.id}
-                taskList={taskList}
-                task={task}
-              />
-            );
-          })}
-        </SortableContext>
-      </DndContext>
-    </section>
-  );
-}
-
-interface TaskListItemComponentProps {
-  taskList: TaskList;
-  task: Task;
-}
-
-function TaskListItemComponent(props: TaskListItemComponentProps) {
-  const taskList = props.taskList;
-  const task = props.task;
-
-  const {
-    attributes,
-    listeners,
-    setNodeRef,
-    setActivatorNodeRef,
-    transform,
-    transition,
-    // isDragging,
-  } = useSortable({ id: task.id });
-
-  const style = {
-    transform: CSS.Transform.toString(transform),
-    transition,
-  };
-
-  return (
-    <div ref={setNodeRef} style={style}>
-      <span ref={setActivatorNodeRef} {...listeners} {...attributes}>
-        H
-      </span>
-      <input
-        type="checkbox"
-        checked={task.completed}
-        onChange={(e) => {
-          const completed = e.currentTarget.checked;
-          updateTask(taskList.id, {
-            ...task,
-            completed,
-          });
-        }}
-      />
-      <TaskTextInput
-        task={task}
-        handleBlur={(_, { text }) => {
-          if (text !== task.text) {
-            updateTask(taskList.id, {
-              ...task,
-              text,
-            });
-          }
-        }}
-      />
-      <input
-        type="date"
-        value={task.date}
-        onChange={(e) => {
-          const date = e.currentTarget.value;
-          updateTask(taskList.id, {
-            ...task,
-            date,
-          });
-        }}
-      />
-    </div>
-  );
-}
+} = {
+  "/home": {
+    isDrawerOpen: false,
+    isSharingSheetOpen: false,
+    isDatePickerSheetOpen: false,
+  },
+  "/menu": {
+    isDrawerOpen: true,
+    isSharingSheetOpen: false,
+    isDatePickerSheetOpen: false,
+  },
+  "/settings": {
+    isDrawerOpen: false,
+    isSharingSheetOpen: false,
+    isDatePickerSheetOpen: false,
+  },
+  "/sharing/:taskListId": {
+    isDrawerOpen: false,
+    isSharingSheetOpen: true,
+    isDatePickerSheetOpen: false,
+  },
+  "/task-lists/:taskListId/tasks/:taskId/date": {
+    isDrawerOpen: false,
+    isSharingSheetOpen: false,
+    isDatePickerSheetOpen: true,
+  },
+};
 
 interface TaskListListItemComponentProps {
   taskList: TaskList;
@@ -336,66 +156,68 @@ export default function AppPage() {
   };
 
   return (
-    <div>
+    <NavigationProvider initialPath="/home" routes={routes}>
       <div>
-        <a href="/app/settings">Settings</a>
-      </div>
-      <div style={{ display: "flex" }}>
-        <ul>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              if (newTaskListName !== "") {
-                insertTaskList(
-                  { name: newTaskListName },
-                  app.taskListIds.length
-                );
-                setNewTaskListName("");
-              }
-            }}
-          >
-            <input
-              type="text"
-              placeholder="New Task List"
-              value={newTaskListName}
-              onChange={(e) => {
-                setNewTaskListName(e.currentTarget.value);
+        <div>
+          <a href="/app/settings">Settings</a>
+        </div>
+        <div style={{ display: "flex" }}>
+          <ul>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                if (newTaskListName !== "") {
+                  insertTaskList(
+                    { name: newTaskListName },
+                    app.taskListIds.length
+                  );
+                  setNewTaskListName("");
+                }
               }}
-            />
-            <button>Add Task List</button>
-          </form>
-          <DndContext
-            sensors={sensors}
-            collisionDetection={closestCenter}
-            modifiers={[restrictToVerticalAxis]}
-            onDragEnd={handleDragEnd}
-          >
-            <SortableContext
-              items={app.taskListIds.map((tlid) => taskLists[tlid])}
-              strategy={verticalListSortingStrategy}
             >
-              {app?.taskListIds.map((tlid) => {
-                return (
-                  taskLists[tlid] && (
-                    <TaskListListItemComponent
-                      key={tlid}
-                      taskList={taskLists[tlid]}
-                      onClick={() => {
-                        setSelectedTaskListId(tlid);
-                      }}
-                    />
-                  )
-                );
-              })}
-            </SortableContext>
-          </DndContext>
-        </ul>
-        <div style={{ flex: 1 }}>
-          {taskList && (
-            <TaskListComponent key={taskList.id} taskList={taskList} />
-          )}
+              <input
+                type="text"
+                placeholder="New Task List"
+                value={newTaskListName}
+                onChange={(e) => {
+                  setNewTaskListName(e.currentTarget.value);
+                }}
+              />
+              <button>Add Task List</button>
+            </form>
+            <DndContext
+              sensors={sensors}
+              collisionDetection={closestCenter}
+              modifiers={[restrictToVerticalAxis]}
+              onDragEnd={handleDragEnd}
+            >
+              <SortableContext
+                items={app.taskListIds.map((tlid) => taskLists[tlid])}
+                strategy={verticalListSortingStrategy}
+              >
+                {app?.taskListIds.map((tlid) => {
+                  return (
+                    taskLists[tlid] && (
+                      <TaskListListItemComponent
+                        key={tlid}
+                        taskList={taskLists[tlid]}
+                        onClick={() => {
+                          setSelectedTaskListId(tlid);
+                        }}
+                      />
+                    )
+                  );
+                })}
+              </SortableContext>
+            </DndContext>
+          </ul>
+          <div style={{ flex: 1 }}>
+            {taskList && (
+              <TaskListComponent key={taskList.id} taskList={taskList} />
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </NavigationProvider>
   );
 }
